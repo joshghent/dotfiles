18513ae06a64e872f9ef95fa93777bb8a021c318
Josh Ghent <me@joshghent.com>
2019-07-08 15:26:22 +0100

    Enable filevault

--- macos.sh
+++ macos.sh
@@ -8,6 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Enable FileVault
+sudo fdesetup enable
+
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
