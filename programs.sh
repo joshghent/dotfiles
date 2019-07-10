21b3af36762d6b934084126214d446fc3dc591e6
Josh Ghent <me@joshghent.com>
2019-07-10 10:51:36 +0100

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -49,8 +49,8 @@ declare -a brews=(
 	"go"
 	"tree"
 	"bat"
-	"php-cs-fixer",
-	"lazydocker",
+	"php-cs-fixer"
+	"lazydocker"
 )
 
 # Install brews in a loop
@@ -96,6 +96,7 @@ declare -a casks=(
 	"keka"
 	"dotnet-sdk"
 	"aerial"
+	"zoomus"
 )
 
 # Install casks in a loop
@@ -116,7 +117,6 @@ valet domain app
 # shellcheck disable=SC2164
 cd ~/Projects && valet park && cd ~
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 brew cleanup
