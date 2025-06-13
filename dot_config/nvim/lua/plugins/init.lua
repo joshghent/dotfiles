74479366a0606e6a73519e48d9eaf9c3296afa97
Josh Ghent <2934976+joshghent@users.noreply.github.com>
2025-06-13 10:19:34 +0100

    added nvim plugins

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,35 +1,92 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufWritePre" },
+		opts = require("configs.treesitter"),
+	},
+	{
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+	{
+		"kaymmm/bullets.nvim",
+		lazy = false,
+		opts = {
+			colon_indent = true,
+			delete_last_bullet = true,
+			empty_buffers = true,
+			file_types = { "markdown", "text", "gitcommit" },
+			line_spacing = 1,
+			mappings = true,
+			outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std*", "std-", "std+" },
+			renumber = true,
+			alpha = {
+				len = 2,
+			},
+			checkbox = {
+				nest = true,
+				markers = " .oOx",
+				toggle_partials = true,
+			},
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 10000,
+		},
+	},
+	{
+		"nvim-orgmode/orgmode",
+		event = "VeryLazy",
+		ft = { "org" },
+		config = function()
+			-- Setup orgmode
+			require("orgmode").setup({
+				org_agenda_files = "~/orgfiles/**/*",
+				org_default_notes_file = "~/orgfiles/refile.org",
+			})
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufWritePre" },
-    opts = require "configs.treesitter",
-  },
-
-  {
-
-    "goolord/alpha-nvim",
-    config = function()
-      require("alpha").setup(require("alpha.themes.dashboard").config)
-    end,
-  },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+			-- add ~org~ to ignore_install
+			-- require('nvim-treesitter.configs').setup({
+			--   ensure_installed = 'all',
+			--   ignore_install = { 'org' },
+			-- })
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
