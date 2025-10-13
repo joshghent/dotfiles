78f5df44c9f5951980321859ad67dd7a618b5e61
Josh Ghent <2934976+joshghent@users.noreply.github.com>
2025-10-13 22:26:32 +0100

    removed cpu and vpn widget, update readme

--- dot_config/sketchybar/items/widgets/init.lua
+++ dot_config/sketchybar/items/widgets/init.lua
@@ -1,6 +1,6 @@
 require("items.widgets.battery")
 require("items.widgets.volume")
 require("items.widgets.wifi")
-require("items.widgets.cpu")
+-- require("items.widgets.cpu")
 require("items.widgets.workspace")
-require("items.widgets.vpn")
+-- require("items.widgets.vpn")
